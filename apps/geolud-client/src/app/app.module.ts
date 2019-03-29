b8c4d83947c5dad181c15bbfe3e5f94548a922d0
fischermatte <fischermatte@posteo.net>
2019-03-29 17:28:32 +0100

    started with material integration

--- apps/geolud-client/src/app/app.module.ts
+++ apps/geolud-client/src/app/app.module.ts
@@ -1,11 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatListModule, MatSidenavModule, MatToolbarModule, MatTooltipModule} from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTooltipModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
